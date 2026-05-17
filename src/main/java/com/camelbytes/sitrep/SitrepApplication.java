6dd683c7482164b2bbbb03e85ea905191c2ca854
Daan Schutte <daanskitte@gmail.com>
2026-05-17 17:51:16 +0300

    Initial commit

---
+++ src/main/java/com/camelbytes/sitrep/SitrepApplication.java
@@ -0,0 +1,13 @@
+package com.camelbytes.sitrep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitrepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SitrepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/camelbytes/sitrep/SitrepApplicationTests.java
@@ -0,0 +1,15 @@
+package com.camelbytes.sitrep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class SitrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
