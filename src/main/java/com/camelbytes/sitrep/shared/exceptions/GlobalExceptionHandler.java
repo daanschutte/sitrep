240a2480bd9cfd36c0af945f49b9ad6de231ded4
Daan Schutte <daanskitte@gmail.com>
2026-05-31 08:37:09 +0300

    add MethodViolation to GlobalExceptionHandler, map to 422

--- src/main/java/com/camelbytes/sitrep/shared/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/camelbytes/sitrep/shared/exceptions/GlobalExceptionHandler.java
@@ -2,18 +2,24 @@ package com.camelbytes.sitrep.shared.exceptions;
 
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Void> handleConstraintViolation() {
-        return ResponseEntity.badRequest().build();
-    }
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<Void> handleConstraintViolation() {
+    return ResponseEntity.unprocessableContent().build();
+  }
 
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Void> handleNotFound() {
-        return ResponseEntity.notFound().build();
-    }
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Void> handleMethodArgumentViolation() {
+    return ResponseEntity.unprocessableContent().build();
+  }
+
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<Void> handleNotFound() {
+    return ResponseEntity.notFound().build();
+  }
 }
