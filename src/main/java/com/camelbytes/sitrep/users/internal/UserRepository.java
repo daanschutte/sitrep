1b8d9e59e5c382d2885f891edbe9ab6bf6d0b62d
Daan Schutte <daanskitte@gmail.com>
2026-05-31 08:37:14 +0300

    formatting

--- src/main/java/com/camelbytes/sitrep/users/internal/UserRepository.java
+++ src/main/java/com/camelbytes/sitrep/users/internal/UserRepository.java
@@ -1,7 +1,6 @@
 package com.camelbytes.sitrep.users.internal;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {}
