460cf74a4d1ec2a1773ee26b57e83d67a1a29944
Daan Schutte <daanskitte@gmail.com>
2026-05-31 08:32:27 +0300

    fix migration, add unique email constraint

--- src/main/resources/db/migration/V01__create_users_table.sql
+++ src/main/resources/db/migration/V01__create_users_table.sql
@@ -1,9 +1,10 @@
 CREATE TABLE users
 (
     id         UUID PRIMARY KEY,
-    firstName  VARCHAR(100) NOT NULL,
-    lastName   VARCHAR(100) NOT NULL,
-    email      VARCHAR(150) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name  VARCHAR(100) NOT NULL,
+    email      VARCHAR(150) NOT NULL UNIQUE,
+    rank       VARCHAR(16)  NOT NULL,
     version    INTEGER,
     created_at TIMESTAMPTZ  NOT NULL,
     updated_at TIMESTAMPTZ  NOT NULL
