5d253465a8a4f71e922c77108dce7427158e2b73
Daan Schutte <daanskitte@gmail.com>
2026-06-01 07:22:40 +0300

    introduce AbstractIntegrationTests

--- src/test/java/com/camelbytes/sitrep/SitrepApplicationTests.java
+++ src/test/java/com/camelbytes/sitrep/SitrepApplicationTests.java
@@ -3,21 +3,10 @@ package com.camelbytes.sitrep;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class SitrepApplicationTests {
-
-  @DynamicPropertySource
-  static void properties(DynamicPropertyRegistry registry) {
-    registry.add("POSTGRES_URL", TestcontainersConfiguration.postgres::getJdbcUrl);
-    registry.add("POSTGRES_USERNAME", TestcontainersConfiguration.postgres::getUsername);
-    registry.add("POSTGRES_PASSWORD", TestcontainersConfiguration.postgres::getPassword);
-    registry.add("APP_USER_USERNAME", TestcontainersConfiguration.postgres::getUsername);
-    registry.add("APP_USER_PASSWORD", TestcontainersConfiguration.postgres::getPassword);
-  }
+class SitrepApplicationTests extends AbstractIntegrationTests {
 
   @Test
   void contextLoads() {}
