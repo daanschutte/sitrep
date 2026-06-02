6aa302da22a940ffd036118609a76e6825de1692
Daan Schutte <daanskitte@gmail.com>
2026-06-02 09:38:18 +0300

    is_active should be not null

--- src/main/resources/db/migration/V03__squadrons.sql
+++ src/main/resources/db/migration/V03__squadrons.sql
@@ -3,7 +3,7 @@ CREATE TABLE squadron
     id         UUID PRIMARY KEY,
     name       VARCHAR(100) NOT NULL,
     short_name VARCHAR(16) UNIQUE,
-    is_active  BOOLEAN DEFAULT TRUE,
+    is_active  BOOLEAN      NOT NULL DEFAULT TRUE,
     version    BIGINT,
     created_at TIMESTAMPTZ  NOT NULL,
     updated_at TIMESTAMPTZ  NOT NULL
