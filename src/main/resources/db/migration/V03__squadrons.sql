d8bb69b8578499f30b855f2813b1e99ab2ccd8e1
Daan Schutte <daanskitte@gmail.com>
2026-06-03 21:47:55 +0300

    add squadron migration

--- src/main/resources/db/migration/V03__squadrons.sql
+++ src/main/resources/db/migration/V03__squadrons.sql
@@ -1,10 +1,10 @@
 CREATE TABLE squadron
 (
     id         UUID PRIMARY KEY,
-    name       VARCHAR(100) NOT NULL,
+    name       VARCHAR(100) UNIQUE NOT NULL,
     short_name VARCHAR(16) UNIQUE,
-    is_active  BOOLEAN      NOT NULL DEFAULT TRUE,
+    is_active  BOOLEAN             NOT NULL DEFAULT TRUE,
     version    BIGINT,
-    created_at TIMESTAMPTZ  NOT NULL,
-    updated_at TIMESTAMPTZ  NOT NULL
+    created_at TIMESTAMPTZ         NOT NULL,
+    updated_at TIMESTAMPTZ         NOT NULL
 );
