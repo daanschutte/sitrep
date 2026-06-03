86e8137dd9a16c6a143f9dfeec857c5b314ed9b8
Daan Schutte <daanskitte@gmail.com>
2026-06-03 21:47:44 +0300

    minor user refactoring

--- src/main/java/com/camelbytes/sitrep/users/api/UserDto.java
+++ src/main/java/com/camelbytes/sitrep/users/api/UserDto.java
@@ -2,4 +2,4 @@ package com.camelbytes.sitrep.users.api;
 
 import java.util.UUID;
 
-public record UserDto(UUID userId, String firstName, String lastName, String email, String rank) {}
+public record UserDto(UUID id, String firstName, String lastName, String email, String rank) {}
--- src/main/java/com/camelbytes/sitrep/users/internal/UserCreateRequest.java
+++ src/main/java/com/camelbytes/sitrep/users/internal/UserCreateRequest.java
@@ -6,5 +6,5 @@ import jakarta.validation.constraints.NotBlank;
 public record UserCreateRequest(
     @NotBlank String firstName,
     @NotBlank String lastName,
-    @Email @NotBlank String email,
+    @NotBlank @Email String email,
     @NotBlank String rank) {}
