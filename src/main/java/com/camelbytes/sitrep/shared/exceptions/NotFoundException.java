bbda95846cdee1288d5db80dbdc82b3b8d4f18da
Daan Schutte <daanskitte@gmail.com>
2026-06-06 10:51:09 +0300

    add simple message case to NotFoundException
    Signed-off-by: Daan Schutte <daanskitte@gmail.com>

--- src/main/java/com/camelbytes/sitrep/shared/exceptions/NotFoundException.java
+++ src/main/java/com/camelbytes/sitrep/shared/exceptions/NotFoundException.java
@@ -1,6 +1,10 @@
 package com.camelbytes.sitrep.shared.exceptions;
 
 public class NotFoundException extends RuntimeException {
+  public NotFoundException(String message) {
+    super(message);
+  }
+
   public NotFoundException(String entity, String id) {
     super(entity + " with id='" + id + "' not found.");
   }
