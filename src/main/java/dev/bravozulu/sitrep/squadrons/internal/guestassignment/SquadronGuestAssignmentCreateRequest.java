f2ee8e555e20ced00399f7cbd87ead5714e062de
Daan Schutte <daanskitte@gmail.com>
2026-09-04 10:19:25 +0200

    Fix validation import
    Signed-off-by: Daan Schutte <daanskitte@gmail.com>

--- src/main/java/dev/bravozulu/sitrep/squadrons/internal/guestassignment/SquadronGuestAssignmentCreateRequest.java
+++ src/main/java/dev/bravozulu/sitrep/squadrons/internal/guestassignment/SquadronGuestAssignmentCreateRequest.java
@@ -2,8 +2,8 @@ package dev.bravozulu.sitrep.squadrons.internal.guestassignment;
 
 import dev.bravozulu.sitrep.squadrons.api.SquadronRole;
 import jakarta.validation.constraints.NotNull;
-import org.jspecify.annotations.NonNull;
 
 import java.util.UUID;
 
-public record SquadronGuestAssignmentCreateRequest(@NotNull UUID userId, @NonNull SquadronRole role) {}
+public record SquadronGuestAssignmentCreateRequest(
+    @NotNull UUID userId, @NotNull SquadronRole role) {}
